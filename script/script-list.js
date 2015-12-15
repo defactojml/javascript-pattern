83a4855651f8d50b601e63969c100f11b5567896
unknown <jean-michel.legrand@S00PTR1868.ca-pacifica.fr>
2015-12-15 17:52:36 +0100

    Improvment

--- script/script-list.js
+++ script/script-list.js
@@ -2,63 +2,119 @@
  * Created by jean-michel.legrand on 17/11/2015.
  */
 
-var cat1 = {
-  name: 'Xuxa',
-  img: 'images/kitty1.jpg',
-  counter: 0
-};
 
+var jsonCats = [{
+  "id": "cat1",
+  "name": "Xuxa",
+  "img": 'images/kitty1.jpg',
+  "counter": 0
+}, {
+  "id": "cat2",
+  "name": "Dramatic",
+  "img": 'images/kitty2.jpg',
+  "counter": 0
+}, {
+  "id": "cat3",
+  "name": "Winter",
+  "img": 'images/kitty3.jpg',
+  "counter": 0
+}, {
+  "id": "cat4",
+  "name": "Japanese",
+  "img": 'images/kitty4.jpg',
+  "counter": 0
+}, {
+  "id": "cat5",
+  "name": "Sweeties",
+  "img": 'images/kitty5.jpg',
+  "counter": 0
+}];
 
-var cat2 = {
-  name: 'Dramatic',
-  img: 'images/kitty2.jpg',
-  counter: 0
-};
-var ulElementMaster = null;
-var ulElementDetail = null;
-
-
-var cats = [];
-cats.push(cat1);
-cats.push(cat2);
-
+var containerElement = document.getElementById('container');
 var masterElement = document.getElementById('master');
 var detailElement = document.getElementById('detail');
 
 
-if (cats.length) {
-  ulElementMaster = document.createElement("ul");
-  ulElementMaster.className = 'cats';
-} else {
 
+// When data are available
+if (jsonCats.length) {
+  // we sort the list
+  var sortedCats = sortByName(jsonCats);
+  // we  build the elements (ul / li + contents) to display the list
+  buildMasterList();
+  detailElement.style.visibility='hidden';
 }
-masterElement.appendChild(ulElementMaster);
-
-cats.forEach(function(cat) {
-  var liElementMaster = null;
-  liElementMaster = document.createElement("li");
-  liElementMaster.innerHTML =   '<a href="#">' + cat.name + '</a>';
-  ulElementMaster.appendChild(liElementMaster);
-});
-
-
-ulElementDetail = document.createElement("ul");
-ulElementDetail.className = 'cat';
-
-detailElement.appendChild(ulElementDetail);
+// When there is no data ...
+else {
+  // we remove the master / detail elements
+  containerElement.removeChild(masterElement);
+  containerElement.removeChild(detailElement);
+  // we create a dedicated message that will get added to the main container element
+  var h2NoCatElement = document.createElement("h2");
+  h2NoCatElement.innerHTML = 'Sorry, no data retrieved';
+  containerElement.appendChild(h2NoCatElement);
+}
 
 
-var liElementDetailName = null, liElementDetailPic = null, liElementDetailCounter = null;
+function sortByName(cats) {
+  var sortedCats = cats.sort(function(catA, catB) {
+    return (catA.name).localeCompare(catB.name);
+  });
+  return sortedCats;
+}
 
-liElementDetailName = document.createElement("li");
-liElementDetailName.innerHTML = "cat's name is " + cat1.name ;
-ulElementDetail.appendChild(liElementDetailName);
 
-liElementDetailPic = document.createElement("li");
-liElementDetailPic.innerHTML = "<img src=" + cat1.img + " />" ;
-"<img src='image.png' alt='The Image' />";
-ulElementDetail.appendChild(liElementDetailPic);
+function buildMasterList() {
 
-liElementDetailCounter = document.createElement("li");
-liElementDetailCounter.innerHTML = "this cat has been clicked " + cat1.counter +  " times" ;
-ulElementDetail.appendChild(liElementDetailCounter);
+  var ulElementMaster = null;
+  ulElementMaster = document.createElement("ul");
+  masterElement.appendChild(ulElementMaster);
+  sortedCats.forEach(function(cat) {
+    var liElementMaster = null, ulElementDetail = null;
+
+    liElementMaster = document.createElement("li");
+    liElementMaster.id = 'textClickageZone';
+    liElementMaster.innerHTML = '<a href="#">' + cat.name + '</a>';
+    ulElementMaster.appendChild(liElementMaster);
+
+    // required to be outside the scope of the 1st addEventListener() because it is used in the 2nd one
+    var liElementDetailCounter = null;
+
+    liElementMaster.addEventListener('click', function () {
+      detailElement.style.visibility='visible';
+      if (ulElementDetail) {
+        detailElement.removeChild(ulElementDetail);
+      }
+      ulElementDetail = document.createElement("ul");
+      detailElement.appendChild(ulElementDetail);
+
+      var liElementDetailName = null, liElementDetailPic = null;
+
+      liElementDetailName = document.createElement("li");
+      liElementDetailName.innerHTML = "cat's name is " + sortedCats[0].name;
+      ulElementDetail.appendChild(liElementDetailName);
+
+      liElementDetailPic = document.createElement("li");
+      liElementDetailPic.innerHTML = "<img id='picClickableZone' src=" + sortedCats[0].img + " />";
+      ulElementDetail.appendChild(liElementDetailPic);
+
+      liElementDetailCounter = document.createElement("li");
+      liElementDetailCounter.innerHTML = "this cat has been clicked " + sortedCats[0].counter + " times";
+      ulElementDetail.appendChild(liElementDetailCounter);
+
+
+      var clickableZoneElement = document.getElementById('picClickableZone');
+      clickableZoneElement.addEventListener('click', function() {
+        sortedCats[0].counter += 1;
+        liElementDetailCounter.innerHTML = "this cat has been clicked " + sortedCats[0].counter + " times";
+      });
+
+    });
+
+
+
+  });
+}
+
+
+
