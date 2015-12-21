e294f9f75a4a0513c17c86ad3fcbc9c75fae16c8
unknown <jean-michel.legrand@S00PTR1868.ca-pacifica.fr>
2015-12-21 18:38:36 +0100

    Refactoring to reinject declaration into html

--- catClickerV4/script/script.js
+++ catClickerV4/script/script.js
@@ -63,35 +63,15 @@ var octopus = {
     return this.getSortedCats().filter(function ( obj ) {
       return obj.id === id;
     })[0];
-
   }
-
-
 };
 
 // The view consists of 3 components: listView, detailView & errorView
 var listView = {
   // initialize the view dom elements
   init: function() {
-    this.containerElement = utils.retrieveContainerElement();
-    this.listElements = createListElements();
-    utils.addElementsToContainer(this.containerElement,this.listElements);
-    listView.render(this.listElements);
-
-    function createListElements(){
-      var mainElement = document.createElement('div');
-      mainElement.setAttribute("id","master");
-      mainElement.setAttribute("class","master");
-
-      var subTitleElement = document.createElement("h2");
-      subTitleElement.innerHTML = 'List of Cats';
-      mainElement.appendChild(subTitleElement);
-
-      var listContainerElement = document.createElement("ul");
-      mainElement.appendChild(listContainerElement);
-
-      return mainElement;
-    }
+    var listContainerElement = document.getElementById('list');
+    listView.render(listContainerElement);
   },
 
   render: function(listElements){
@@ -100,13 +80,12 @@ var listView = {
       listElement = document.createElement("li");
       listElement.id = cat.id ;
       listElement.innerHTML = '<a href="#">' + cat.name + '</a>';
-      listElements.childNodes[1].appendChild(listElement);
-
+      listElements.appendChild(listElement);
 
       listElement.addEventListener('click', function(e) {
         var catId = this.id;
         var cat = octopus.getCatById(catId);
-        detailView.render(detailView.detailElements.childNodes[1], cat);
+        detailView.render(cat);
       });
     });
   }
@@ -115,141 +94,58 @@ var listView = {
 var detailView = {
   // initialize the view dom elements
   init: function() {
-    this.containerElement = utils.retrieveContainerElement();
-    this.detailElements = createDetailElements();
-    utils.addElementsToContainer(this.containerElement,this.detailElements);
-    detailView.render(this.detailElements.childNodes[1], octopus.getFirstCatFromSortedCats());
-
-    function createDetailElements(){
-      var mainElement = document.createElement('div');
-      mainElement.setAttribute("id","detail");
-      mainElement.setAttribute("class","detail");
-
-      var subTitleElement = document.createElement("h2");
-      subTitleElement.innerHTML = 'Detail of the Cat selected';
-      mainElement.appendChild(subTitleElement);
-
-      var detailContainerElement = document.createElement("ul");
-      mainElement.appendChild(detailContainerElement);
-
-      var liElementDetailName = document.createElement("li");
-      detailContainerElement.appendChild(liElementDetailName);
-
-      var liElementDetailPic = document.createElement("li");
-      detailContainerElement.appendChild(liElementDetailPic);
-
-      var liElementDetailCounter = document.createElement("li");
-      detailContainerElement.appendChild(liElementDetailCounter);
-
-      return mainElement
-    }
+    var detailContainerElement = document.getElementById('detailCat');
+    detailView.render(octopus.getFirstCatFromSortedCats());
   },
-  render: function(catDetailElements, cat) {
-    catDetailElements.childNodes[0].setAttribute("id", cat.id);
-    catDetailElements.childNodes[0].innerHTML = "cat's name is " + cat.name;
-    catDetailElements.childNodes[1].innerHTML = "<img id='picClickableZone' src=" + cat.img + " />";
-    catDetailElements.childNodes[2].innerHTML = "this cat has been clicked " + cat.counter + " times";
+  render: function(cat) {
+    var detailCatName = document.getElementById('detailCatName');
+    detailCatName.setAttribute("catid", cat.id);
+    detailCatName.innerHTML = "cat's name is " + cat.name;
 
-    var clickableZoneElement = document.getElementById('picClickableZone');
-    clickableZoneElement.addEventListener('click', function(e) {
-      var catId = this.parentElement.parentElement.childNodes[0].id;
+    var detailCatImg = document.getElementById('detailCatImg');
+    detailCatImg.innerHTML = "<img id='picClickableZone' src=" + cat.img + " />";
+
+    var detailCatCounter = document.getElementById('detailCatCounter');
+    detailCatCounter.innerHTML = "this cat has been clicked " + cat.counter + " times";
+
+    detailCatImg.addEventListener('click', function(e) {
+      var catId = detailCatName.getAttribute("catid");
       var cat = octopus.getCatById(catId);
       cat.counter += 1;
-      this.parentElement.parentElement.childNodes[2].innerHTML = "this cat has been clicked " + cat.counter + " times";
+      detailCatCounter.innerHTML = "this cat has been clicked " + cat.counter + " times";
     });
   }
 };
 
 var errorView = {
   init: function () {
-    this.containerElement = utils.retrieveContainerElement();
-    this.errorElements = createErrorElements();
-    utils.addElementsToContainer(this.containerElement,this.errorElements);
-    errorView.render(this.errorElements);
-
-    function createErrorElements(){
-      return document.createElement("h2")
-    }
+    var masterElement = document.getElementById('master');
+    var detailElement = document.getElementById('detail');
+    var adminElement = document.getElementById('admin');
+    var errorElement = document.getElementById('error');
+    errorView.render(masterElement, detailElement, adminElement, errorElement);
   },
-  render: function(errorElements) {
-    errorElements.innerHTML = 'Sorry, no data retrieved';
+  render: function(masterElement, detailElement, adminElement, errorElement ) {
+    masterElement.parentNode.removeChild(masterElement);
+    detailElement.parentNode.removeChild(detailElement);
+    adminElement.parentNode.removeChild(adminElement);
+    errorElement.style.visibility = "visible";
   }
 };
 
 
 var adminView = {
   init: function () {
-    this.containerElement = utils.retrieveContainerElement();
-    this.adminElements = createAdminElements();
-    utils.addElementsToContainer(this.containerElement,this.adminElements);
-    adminView.render(this.adminElements);
-
-    function createAdminElements(){
-      var mainElement = document.createElement('div');
-      mainElement.setAttribute("id", "admin");
-      mainElement.setAttribute("class", "admin");
-
-      var subTitleElement = document.createElement("h2");
-      subTitleElement.innerHTML = 'Admin section';
-      mainElement.appendChild(subTitleElement);
-
-      var subSectionElement1 = document.createElement("div");
-      subSectionElement1.setAttribute("id", "header");
-      subSectionElement1.setAttribute("class", "header");
-      mainElement.appendChild(subSectionElement1);
-
-      var buttonElement = document.createElement("input");
-      buttonElement.type = "button";
-      buttonElement.value = "admin";
-      subSectionElement1.appendChild(buttonElement);
-
-      var subSectionElement2 = document.createElement("div");
-      subSectionElement2.setAttribute("id", "adminDetail");
-      subSectionElement2.setAttribute("class", "adminDetail");
-      mainElement.appendChild(subSectionElement2);
-
-
-      var detailContainerElement = document.createElement("ul");
-      subSectionElement2.appendChild(detailContainerElement);
-
-      var nameElementDetail = document.createElement("li");
-      var nameLabelValue = document.createElement("span");
-      nameLabelValue.innerHTML = " Name of the cat";
-      var nameInputValue = document.createElement("input");
-      nameElementDetail.appendChild(nameLabelValue);
-      nameElementDetail.appendChild(nameInputValue);
-      detailContainerElement.appendChild(nameElementDetail);
-
-      var counterElementDetail = document.createElement("li");
-      var counterLabelValue = document.createElement("span");
-      counterLabelValue.innerHTML = " Number of # for the cat";
-      var counterInputValue = document.createElement("input");
-      counterElementDetail.appendChild(counterLabelValue);
-      counterElementDetail.appendChild(counterInputValue);
-      detailContainerElement.appendChild(counterElementDetail);
-
-      var submitElementDetail = document.createElement("li");
-      var buttonSubmitElement = document.createElement("input");
-      buttonSubmitElement.type = "button";
-      buttonSubmitElement.value = "submit";
-      submitElementDetail.appendChild(buttonSubmitElement);
-      detailContainerElement.appendChild(submitElementDetail);
-
-      return mainElement;
-    }
+    adminView.render();
   },
-  render: function(adminElements) {
+  render: function() {
+    var adminDetailElement = document.getElementById('adminDetail');
+    adminDetailElement.style.visibility='hidden';
   }
 
 };
 
 var utils = {
-  addElementsToContainer: function(containerElement, element){
-    containerElement.appendChild(element);
-  },
-  retrieveContainerElement: function() {
-    return document.getElementById('container');
-  }
 };
 
 octopus.init();
