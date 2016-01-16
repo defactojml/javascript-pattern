54011293a91528c47063384fba1dc8ea2e91ebea
jmllegrand <jml.legrand@gmail.com>
2016-01-16 19:26:10 +0100

    from home witht the brand new imac

--- catClickerV1/script/script.js
+++ catClickerV1/script/script.js
@@ -3,49 +3,49 @@
  */
 
 
-// récuperation des div englobantes pour gerer la visibilité
+// get the 2 main div to handle visibility
 var cat1El = document.getElementById('cat1');
 var cat2El = document.getElementById('cat2');
 
-// récuperation des images
+// get the images
 var kitty1 = document.getElementById('kitty1');
 var kitty2 = document.getElementById('kitty2');
 
-// récuperation des compteurs associés aux chats
+// get the counters associated to the cats
 var increment1El = document.getElementById('increment1');
 var increment2El = document.getElementById('increment2');
 
-// récuperation des noms associés aux chats
+// get the names associated to the cats
 var name1El = document.getElementById('name1');
 var name2El = document.getElementById('name2');
 
-// au chargement, on cache ses div
-cat1El.style.visibility='hidden';
-cat2El.style.visibility='hidden';
+//by default, we hide the divs
+cat1El.style.visibility = 'hidden';
+cat2El.style.visibility = 'hidden';
 
-// initialisation des compteurs
+// we initialize the counters
 var increment1 = 0, increment2 = 0;
 
-// ajout des events listeners pour gerer le click souris
+// add the events listeners to handle the click
 // vanilla JS style with no jQuery
 kitty1.addEventListener('click', function () {
-  increment1 += 1;
-  if (increment1 === 1) {
-    // pour le 1er coup, on rend visible le commentaire et on set le nom
-    cat1El.style.visibility='visible';
-    name1El.innerHTML = 'cat1';
-  }
-  increment1El.innerHTML = increment1;
+    increment1 += 1;
+    if (increment1 === 1) {
+        // pour le 1er coup, on rend visible le commentaire et on set le nom
+        cat1El.style.visibility = 'visible';
+        name1El.innerHTML = 'cat1';
+    }
+    increment1El.innerHTML = increment1;
 }, false);
 
 kitty2.addEventListener('click', function () {
-  increment2 += 1;
-  if (increment2 === 1) {
-    // pour le 1er coup, on rend visible le commentaire et on set le nom
-    cat2El.style.visibility='visible';
-    name2El.innerHTML = 'cat2';
-  }
-  increment2El.innerHTML = increment2;
+    increment2 += 1;
+    if (increment2 === 1) {
+        // pour le 1er coup, on rend visible le commentaire et on set le nom
+        cat2El.style.visibility = 'visible';
+        name2El.innerHTML = 'cat2';
+    }
+    increment2El.innerHTML = increment2;
 }, false);
 
 
