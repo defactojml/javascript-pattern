87d683e7aa29ffbcc6e35fddaddd9dc7d639b531
jean-michel.legrand <Fluent2015>
2016-01-19 10:51:11 +0100

    Improved version from catClickerV2 learnings

--- catClickerV3/js/app.js
+++ catClickerV3/js/app.js
@@ -2,38 +2,10 @@
  * Created by jean-michel.legrand on 17/11/2015.
  */
 
-// mocked json file
-var jsonCats = [{
-  "id": "cat1",
-  "name": "Xuxa",
-  "img": 'images/kitty1.jpg',
-  "counter": 0
-}, {
-  "id": "cat2",
-  "name": "Dramatic",
-  "img": 'images/kitty2.jpg',
-  "counter": 0
-}, {
-  "id": "cat3",
-  "name": "Winter",
-  "img": 'images/kitty3.jpg',
-  "counter": 0
-}, {
-  "id": "cat4",
-  "name": "Japanese",
-  "img": 'images/kitty4.jpg',
-  "counter": 0
-}, {
-  "id": "cat5",
-  "name": "Sweeties",
-  "img": 'images/kitty5.jpg',
-  "counter": 0
-}];
-
 var model = {
   getAllCats: function() {
-    if (jsonCats.length) {
-      return jsonCats;
+    if (datas.length) {
+      return datas;
     }
   }
 };
