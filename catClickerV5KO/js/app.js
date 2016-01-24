47c8033fafadf7ec632dd94495347fe0ddce4cdc
jmllegrand <jml.legrand@gmail.com>
2016-01-24 16:43:50 +0100

    catClickerV5 add the dataBinding + add properties into the VM

--- catClickerV5KO/js/app.js
+++ catClickerV5KO/js/app.js
@@ -1,10 +1,15 @@
 var viewModel = function () {
-  this.counter = ko.observable(0);
+  var juniorThreshold = 5;
+
   this.name = ko.observable("Dramatic");
   this.imgSrc = ko.observable("images/kitty2.jpg");
+  this.counter = ko.observable(0);
+  this.level = ko.observable("Newborn");
+
 
   this.incrementCounter = function () {
-    this.clickCount(this.clickCount + 1);
+    this.counter(this.counter() + 1);
+    this.counter() >= juniorThreshold ? this.level("Infant") : this.level();
   };
 
 };
